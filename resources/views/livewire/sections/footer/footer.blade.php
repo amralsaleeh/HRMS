7492f00de8c680e616521e1cb9f2916d08fc5298
Amr Alsaleh <amralsaleeh@outlook.com>
2023-10-20 23:01:57 +0300

    Customize the footer

--- resources/views/livewire/sections/footer/footer.blade.php
+++ resources/views/livewire/sections/footer/footer.blade.php
@@ -8,13 +8,11 @@
             document.write(new Date().getFullYear())
 
           </script>
-          , made with ❤️ by <a href="{{ (!empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '') }}" target="_blank" class="fw-semibold">{{ (!empty(config('variables.creatorName')) ? config('variables.creatorName') : '') }}</a>
+          , made with ❤️ by <a href="http://namaa.sy/" target="_blank" class="fw-semibold">IT Department</a>
         </div>
         <div>
-          <a href="{{ config('variables.licenseUrl') ? config('variables.licenseUrl') : '#' }}" class="footer-link me-4" target="_blank">License</a>
-          <a href="{{ config('variables.moreThemes') ? config('variables.moreThemes') : '#' }}" target="_blank" class="footer-link me-4">More Themes</a>
-          <a href="{{ config('variables.documentation') ? config('variables.documentation') : '#' }}" target="_blank" class="footer-link me-4">Documentation</a>
-          <a href="{{ config('variables.support') ? config('variables.support') : '#' }}" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+          <a href="https://data.namaa.sy/" target="_blank" class="footer-link me-4">Data system</a>
+          <a href="http://namaa.sy/" target="_blank" class="footer-link d-none d-sm-inline-block">Namaa</a>
         </div>
       </div>
     </div>
