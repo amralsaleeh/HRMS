5400657a5367724c6401e143659f05a5f221ea1c
Amr Alsaleh <amralsaleeh@outlook.com>
2023-10-20 16:29:47 +0300

    Delete unnecessary rows

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,3 @@ Route::get('/', $controller_path.'\pages\HomePage@index')->name('pages-home');
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
 });
-
-// // pages
-// Route::get('/page-2', $controller_path.'\pages\Page2@index')->name('pages-page-2');
-// Route::get('/pages/misc-error', $controller_path.'\pages\MiscError@index')->name('pages-misc-error');
-
-// // authentication
-// Route::get('/auth/login-basic', $controller_path.'\authentications\LoginBasic@index')->name('auth-login-basic');
-// Route::get('/auth/register-basic', $controller_path.'\authentications\RegisterBasic@index')->name(
-//     'auth-register-basic'
-// );
-
-// Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
