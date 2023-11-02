30f644da339250d6a97abd693d55b36475720f74
Sewar Khalil <sewar.khalil92@gmail.com>
2023-11-02 15:21:47 +0300

    added migration files

---
+++ database/migrations/2023_11_02_114106_create_leaves_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('discount_rate')->length(3);
+            $table->longText('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leaves');
+    }
+};
---
+++ database/migrations/2023_11_02_121209_create_centers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('centers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->time('start_hour_point');
+            $table->time('end_hour_point');
+            $table->string('weekends');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('centers');
+    }
+};
