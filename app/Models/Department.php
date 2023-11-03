12f8101bbc6100ad880e47b012179f7d74bf2504
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-03 20:50:36 +0300

    Update and rename Departement.php to Department.php

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Departement extends Model
+class Department extends Model
 {
     use HasFactory;
 }
