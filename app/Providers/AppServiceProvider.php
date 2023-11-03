9aaaef936dbb34913460bf983ee97616b7aea0eb
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-04 02:23:58 +0300

    Set start of the weekday

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,29 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-  /**
-   * Register any application services.
-   */
-  public function register(): void
-  {
-    //
-  }
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-  /**
-   * Bootstrap any application services.
-   */
-  public function boot(): void
-  {
-    //
-  }
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Carbon::setWeekStartsAt(Carbon::SUNDAY);
+
+        Carbon::setWeekendDays([
+            Carbon::FRIDAY,
+            Carbon::SATURDAY,
+        ]);
+    }
 }
