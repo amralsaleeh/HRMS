37f1b78c6075dc523f0b33fcb210a7c7f499df90
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-06 19:18:11 +0300

    Enable TODO & FIXME tags

---
+++ #Project_Tasks.php
@@ -0,0 +1,7 @@
+<?php
+
+// TODO: Add softDeletes to all migration files.
+// TODO: Add CreatedUpdatedBy, softDeletes trait to all models files.
+// TODO: Set isRtl true after change lang to Arabic.
+// TODO: Enable roles and permissions.
+// TODO: Translate all strings to Arabic.
