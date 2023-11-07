9454a6b6fe4da9ee56d0dac0cc276af905a49472
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-07 10:41:03 +0300

    Enable deleted_by column to the trait

--- app/Traits/CreatedUpdatedDeletedBy.php
+++ app/Traits/CreatedUpdatedDeletedBy.php
@@ -2,11 +2,11 @@
 
 namespace App\Traits;
 
-trait CreatedUpdatedBy
+trait CreatedUpdatedDeletedBy
 {
-    public static function bootCreatedUpdatedBy()
+    public static function bootCreatedUpdatedDeletedBy()
     {
-        // updating created_by and updated_by when model is created
+        // Updating created_by and updated_by when model is created
         static::creating(function ($model) {
             if (! $model->isDirty('created_by')) {
                 $model->created_by = auth()->user()->name;
@@ -16,10 +16,18 @@ trait CreatedUpdatedBy
             }
         });
 
-        // updating updated_by when model is updated
+        // Updating updated_by when model is updated
         static::updating(function ($model) {
             if (! $model->isDirty('updated_by')) {
                 $model->updated_by = auth()->user()->name;
+            }
+        });
+
+        // Updating deleted_by when is deleting
+        static::deleting(function ($model) {
+            if (! $model->isDirty('deleted_by')) {
+                $model->deleted_by = auth()->user()->name;
+                $model->update(['deleted_by' => auth()->user()->name]);
             }
         });
     }
