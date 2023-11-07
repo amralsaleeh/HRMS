bf1c2d6e27c829896e44914931cc16b881e485e5
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-07 11:54:22 +0300

    Update #Project_Tasks

--- #Project_Tasks.php
+++ #Project_Tasks.php
@@ -1,6 +1,5 @@
 <?php
 
-// TODO: Add CreatedUpdatedBy, softDeletes trait to all models files.
 // TODO: Set isRtl true after change lang to Arabic.
 // TODO: Enable roles and permissions.
 // TODO: Translate all strings to Arabic.
