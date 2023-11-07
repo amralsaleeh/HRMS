b5066adfa9cddb9309e89ef8ea3139435b0db91f
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-07 12:21:25 +0300

    Update #Project_Tasks

--- #Project_Tasks.php
+++ #Project_Tasks.php
@@ -1,5 +1,9 @@
 <?php
 
+// Development (under construction)
 // TODO: Set isRtl true after change lang to Arabic.
 // TODO: Enable roles and permissions.
 // TODO: Translate all strings to Arabic.
+
+// Deployment (before publishing to the hosting server)
+// TODO: Make sure that log-viewer is enabled for admins only.
