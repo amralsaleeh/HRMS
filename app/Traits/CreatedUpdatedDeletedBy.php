c798699356a84a0748adfd8aa1503b0cf07e77c5
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-08 16:07:00 +0300

    Check auth

--- app/Traits/CreatedUpdatedDeletedBy.php
+++ app/Traits/CreatedUpdatedDeletedBy.php
@@ -6,29 +6,56 @@ trait CreatedUpdatedDeletedBy
 {
     public static function bootCreatedUpdatedDeletedBy()
     {
-        // Updating created_by and updated_by when model is created
-        static::creating(function ($model) {
-            if (! $model->isDirty('created_by')) {
-                $model->created_by = auth()->user()->name;
-            }
-            if (! $model->isDirty('updated_by')) {
-                $model->updated_by = auth()->user()->name;
-            }
-        });
+        if (auth()->user()) {
+            // Updating created_by and updated_by when model is created
+            static::creating(function ($model) {
+                if (! $model->isDirty('created_by')) {
+                    $model->created_by = auth()->user()->name;
+                }
+                if (! $model->isDirty('updated_by')) {
+                    $model->updated_by = auth()->user()->name;
+                }
+            });
 
-        // Updating updated_by when model is updated
-        static::updating(function ($model) {
-            if (! $model->isDirty('updated_by')) {
-                $model->updated_by = auth()->user()->name;
-            }
-        });
+            // Updating updated_by when model is updated
+            static::updating(function ($model) {
+                if (! $model->isDirty('updated_by')) {
+                    $model->updated_by = auth()->user()->name;
+                }
+            });
 
-        // Updating deleted_by when is deleting
-        static::deleting(function ($model) {
-            if (! $model->isDirty('deleted_by')) {
-                $model->deleted_by = auth()->user()->name;
-                $model->update(['deleted_by' => auth()->user()->name]);
-            }
-        });
+            // Updating deleted_by when is deleting
+            static::deleting(function ($model) {
+                if (! $model->isDirty('deleted_by')) {
+                    $model->deleted_by = auth()->user()->name;
+                    $model->update(['deleted_by' => auth()->user()->name]);
+                }
+            });
+        } else {
+            // Updating created_by and updated_by when model is created
+            static::creating(function ($model) {
+                if (! $model->isDirty('created_by')) {
+                    $model->created_by = 'IT Department';
+                }
+                if (! $model->isDirty('updated_by')) {
+                    $model->updated_by = 'IT Department';
+                }
+            });
+
+            // Updating updated_by when model is updated
+            static::updating(function ($model) {
+                if (! $model->isDirty('updated_by')) {
+                    $model->updated_by = 'IT Department';
+                }
+            });
+
+            // Updating deleted_by when is deleting
+            static::deleting(function ($model) {
+                if (! $model->isDirty('deleted_by')) {
+                    $model->deleted_by = 'IT Department';
+                    $model->update(['deleted_by' => 'IT Department']);
+                }
+            });
+        }
     }
 }
