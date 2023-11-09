eca68f8871f09ac18ecfc622865c4834c9f37e8d
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-09 11:07:38 +0300

    Finalized department page

--- app/Livewire/HumanResource/Structure/Departments.php
+++ app/Livewire/HumanResource/Structure/Departments.php
@@ -2,16 +2,97 @@
 
 namespace App\Livewire\HumanResource\Structure;
 
+use App\Models\Department;
+use App\Models\Timeline;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Departments extends Component
 {
     // Variables - Start //
     public $departments = [];
-    // Variables - Start //
+
+    public $department;
+
+    #[Rule('required')]
+    public $name;
+
+    public $is_edit = false;
+
+    public $confirmedId;
+    // Variables - End //
 
     public function render()
     {
+        $this->departments = Department::all();
+
         return view('livewire.human-resource.structure.departments');
+    }
+
+    public function submitDepartment()
+    {
+        $this->is_edit ? $this->editDepartment() : $this->addDepartment();
+    }
+
+    public function addDepartment()
+    {
+        $this->validate();
+
+        Department::create([
+            'name' => $this->name,
+        ]);
+
+        $this->dispatch('closeModal', elementId: '#departmentModal');
+        $this->dispatch('toastr', type: 'success'/* , title: 'Done!' */ , message: 'Going Well!');
+    }
+
+    public function editDepartment()
+    {
+        $this->validate();
+
+        $this->department->update([
+            'name' => $this->name,
+        ]);
+
+        $this->dispatch('closeModal', elementId: '#departmentModal');
+        $this->dispatch('toastr', type: 'success'/* , title: 'Done!' */ , message: 'Going Well!');
+
+        $this->reset();
+    }
+
+    public function confirmDeleteDepartment($id)
+    {
+        $this->confirmedId = $id;
+    }
+
+    public function deleteDepartment(Department $department)
+    {
+        $department->delete();
+        $this->dispatch('toastr', type: 'success'/* , title: 'Done!' */ , message: 'Going Well!');
+    }
+
+    public function showNewDepartmentModal()
+    {
+        $this->reset();
+    }
+
+    public function showEditDepartmentModal(Department $department)
+    {
+        $this->reset();
+        $this->is_edit = true;
+
+        $this->department = $department;
+
+        $this->name = $department->name;
+    }
+
+    public function getCoordinator($id)
+    {
+        //
+    }
+
+    public function getMembersCount($department_id)
+    {
+        return Timeline::where('department_id', $department_id)->whereNull('end_date')->distinct('employee_id')->count();
     }
 }
--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\CreatedUpdatedDeletedBy;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -16,10 +17,18 @@ class Department extends Model
         'name',
         'created_by',
         'updated_by',
+        'deleted_by',
     ];
 
     public function timelines(): HasMany
     {
         return $this->hasMany(Timeline::class);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => ucfirst($value),
+        );
     }
 }
