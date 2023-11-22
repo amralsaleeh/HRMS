d263745fde0259094bd74ecbb57c18dfb05d132f
Amr Alsaleh <amralsaleeh@outlook.com>
2023-11-22 09:20:24 +0300

    Install maatwebsite/excel package

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -56,6 +56,17 @@
       'Livewire' => 'Livewire\\Livewire',
     ),
   ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
   'nesbot/carbon' => 
   array (
     'providers' => 
