b207a5029769db25a1c47daa90bb8f676cc922b9
Amr Alsaleh <amralsaleeh@outlook.com>
2023-12-15 03:22:49 +0300

    Install arabic font

--- resources/views/layouts/sections/styles.blade.php
+++ resources/views/layouts/sections/styles.blade.php
@@ -1,9 +1,17 @@
 <!-- BEGIN: Theme CSS-->
 <!-- Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+{{-- <link rel="preconnect" href="https://fonts.googleapis.com"> --}}
+{{-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
+{{-- <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"> --}}
+{{-- <link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"> --}}
+<style>
+@font-face{
+	font-family: Noto Kufi Arabic;
+	font-weight: normal;
+    src:
+        url(assets/fonts/NotoKufiArabic-VariableFont_wght.ttf) format('truetype');
+    }
+</style>
 
 <link rel="stylesheet" href="{{ asset(mix('assets/vendor/fonts/fontawesome.css')) }}" />
 <link rel="stylesheet" href="{{ asset(mix('assets/vendor/fonts/tabler-icons.css')) }}" />
