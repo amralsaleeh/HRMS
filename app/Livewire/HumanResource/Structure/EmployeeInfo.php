931362ffbf1e01530c51aef7c65eee509097ad3b
Amr Alsaleh <amralsaleeh@outlook.com>
2024-03-28 13:00:54 +0300

    Update employee page functions

--- app/Livewire/HumanResource/Structure/EmployeeInfo.php
+++ app/Livewire/HumanResource/Structure/EmployeeInfo.php
@@ -2,20 +2,75 @@
 
 namespace App\Livewire\HumanResource\Structure;
 
+use App\Models\Center;
+use App\Models\Department;
 use App\Models\Employee;
+use App\Models\Position;
+use App\Models\Timeline;
 use Livewire\Component;
 
 class EmployeeInfo extends Component
 {
     public $employee;
 
+    // public $employee_id;
+    public $timelines;
+
+    public $position;
+
+    public $employeePosition;
+
+    public $positions;
+
+    public $Centers;
+
+    public $employeeCenter;
+
+    public $departments;
+
+    public $employeeDepartment;
+
+    public $center;
+
+    public $startDate;
+
+    public $quitDate;
+
     public function mount($id)
     {
         $this->employee = Employee::find($id);
+        $this->timelines = Timeline::with(['center', 'department', 'position'])->where('employee_id', $id)->get();
+        // dd($this->timeline);
+        $this->position = Timeline::with('position')->where('employee_id', $id)->first();
+        $this->positions = Position::all();
+        $this->Centers = Center::all();
+        $this->departments = department::all();
+        $this->dispatch('closeModal', elementId: '#employeeinfoModal');
+        $this->dispatch('toastr', type: 'success'/* , title: 'Done!' */ , message: 'Going Well!');
+
+        // dd($this->posation->start_date);
     }
 
     public function render()
     {
         return view('livewire.human-resource.structure.employee-info');
+    }
+
+    public function submitTimeline()
+    {
+        // $this->validate();
+
+        Timeline::create([
+            'employee_id' => $this->employee->id,
+            'center_id' => $this->employeeCenter,
+            'department_id' => $this->employeeDepartment,
+            'position_id' => $this->employeePosition,
+            'start_date' => $this->startDate,
+            'end_date' => $this->quitDate,
+
+        ]);
+
+        $this->dispatch('closeModal', elementId: '#centerModal');
+        $this->dispatch('toastr', type: 'success'/* , title: 'Done!' */ , message: 'Going Well!');
     }
 }
