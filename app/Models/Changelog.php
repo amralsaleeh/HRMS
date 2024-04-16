d99fe701d7fb05e9854e02608942e659caf29220
Amr Alsaleh <amralsaleeh@outlook.com>
2024-04-16 10:49:14 +0300

    Create changelogs table and model

---
+++ app/Models/Changelog.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\CreatedUpdatedDeletedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Changelog extends Model
+{
+    use CreatedUpdatedDeletedBy, HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'version',
+        'title',
+        'description',
+    ];
+}
