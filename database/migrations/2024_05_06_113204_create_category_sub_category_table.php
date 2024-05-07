cdcc082c9e9bc1c1961c4a9841c30baf3881a092
Amr Alsaleh <amralsaleeh@outlook.com>
2024-05-07 09:40:19 +0300

    Create "category_sub_category" pivot table

---
+++ database/migrations/2024_05_06_113204_create_category_sub_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_sub_category', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('sub_category_id')->constrained();
+            $table->primary(['category_id', 'sub_category_id']);
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->string('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_sub_category');
+    }
+};
