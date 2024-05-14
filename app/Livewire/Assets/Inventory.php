9c0e7935be0a3d3d266c5c88fbecafe9401e440e
Amr Alsaleh <amralsaleeh@outlook.com>
2024-05-14 11:22:32 +0300

    Set the inventory page

--- app/Livewire/Assets/Inventory.php
+++ app/Livewire/Assets/Inventory.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire\Assets;
 
+use App\Models\Asset;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Inventory extends Component
 {
+    use WithPagination;
+
+    // 👉 Variables
+    public $search_term = null;
+
+    public $colors = [
+        'Good' => 'success',
+        'Fine' => 'primary',
+        'Bad' => 'warning',
+        'Damaged' => 'danger',
+    ];
+
     public function render()
     {
-        return view('livewire.assets.inventory');
+        $assets = Asset::where('id', 'like', '%'.$this->search_term.'%')
+            ->orWhere('old_id', 'like', '%'.$this->search_term.'%')
+            ->paginate(20);
+
+        return view('livewire.assets.inventory', [
+            'assets' => $assets, ]);
     }
 }
--- resources/views/livewire/assets/inventory.blade.php
+++ resources/views/livewire/assets/inventory.blade.php
@@ -1,126 +1,62 @@
 <div class="card">
-  <h5 class="card-header">Hoverable rows</h5>
+  <div class="card-header d-flex justify-content-between">
+    <h5 class="card-title m-0 me-2">Assets</h5>
+    <div class="col-4">
+      <input wire:model.live="search_term" type="text" class="form-control" placeholder="Search (ID, Old ID...)">
+    </div>
+  </div>
   <div class="table-responsive text-nowrap">
     <table class="table table-hover">
       <thead>
         <tr>
-          <th>Project</th>
-          <th>Client</th>
-          <th>Users</th>
+          <th>ID</th>
+          <th>Old ID</th>
+          <th>Serial Number</th>
           <th>Status</th>
           <th>Actions</th>
         </tr>
       </thead>
       <tbody class="table-border-bottom-0">
-        <tr>
-          <td><i class="ti ti-brand-angular ti-lg text-danger me-3"></i> <strong>Angular Project</strong></td>
-          <td>Albert Cook</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Lilian Fuller" data-bs-original-title="Lilian Fuller">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Sophia Wilkerson" data-bs-original-title="Sophia Wilkerson">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Christina Parker" data-bs-original-title="Christina Parker">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-primary me-1">Active</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
+        @forelse ($assets as $asset)
+          <tr>
+            <td><i class="ti ti-tag ti-sm text-danger me-3"></i> <strong>{{ $asset->id }}</strong></td>
+            <td>{{ $asset->old_id }}</td>
+            <td>{{ $asset->serial_number }}</td>
+            <td><span class="badge bg-label-{{ $colors[$asset->status] }} me-1">{{ $asset->status }}</span></td>
+            <td>
+              <div class="dropdown">
+                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
+                  <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
+                </div>
               </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td><i class="ti ti-brand-react-native ti-lg text-info me-3"></i> <strong>React Project</strong></td>
-          <td>Barry Hunter</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Lilian Fuller" data-bs-original-title="Lilian Fuller">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Sophia Wilkerson" data-bs-original-title="Sophia Wilkerson">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Christina Parker" data-bs-original-title="Christina Parker">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-success me-1">Completed</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
+            </td>
+          </tr>
+        @empty
+          <tr>
+            <td colspan="5">
+              <div class="mt-2 mb-2" style="text-align: center">
+                  <h3 class="mb-1 mx-2">Oopsie-doodle!</h3>
+                  <p class="mb-4 mx-2">
+                    No data found, please sprinkle some data in my virtual bowl, and let the fun begin!
+                  </p>
+                  <button class="btn btn-label-primary mb-4" data-bs-toggle="modal" data-bs-target="#assetModal">
+                      Add New Asset
+                    </button>
+                  <div>
+                    <img src="{{ asset('assets/img/illustrations/page-misc-under-maintenance.png') }}" width="200" class="img-fluid">
+                  </div>
               </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td><i class="ti ti-brand-vue ti-lg text-success me-3"></i> <strong>VueJs Project</strong></td>
-          <td>Trevor Baker</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Lilian Fuller" data-bs-original-title="Lilian Fuller">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Sophia Wilkerson" data-bs-original-title="Sophia Wilkerson">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Christina Parker" data-bs-original-title="Christina Parker">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-info me-1">Scheduled</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td><i class="ti ti-brand-bootstrap ti-lg text-primary me-3"></i> <strong>Bootstrap Project</strong></td>
-          <td>Jerry Milton</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Lilian Fuller" data-bs-original-title="Lilian Fuller">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Sophia Wilkerson" data-bs-original-title="Sophia Wilkerson">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" aria-label="Christina Parker" data-bs-original-title="Christina Parker">
-                <img src="http://v-bootstrab-laravel.test/assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-warning me-1">Pending</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
-              </div>
-            </div>
-          </td>
-        </tr>
+            </td>
+          </tr>
+        @endforelse
       </tbody>
     </table>
   </div>
+
+  <div class="row mt-4">
+    {{ $assets->links() }}
+  </div>
+
 </div>
