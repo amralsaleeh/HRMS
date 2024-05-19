f33879a69f67f8262f86e65ed6e0f3f8387bc3b3
Amr Alsaleh <amralsaleeh@outlook.com>
2024-05-19 11:06:17 +0300

    Update table nullable columns

--- database/migrations/2024_04_30_115612_create_assets_table.php
+++ database/migrations/2024_04_30_115612_create_assets_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('assets', function (Blueprint $table) {
             $table->string('id');
             $table->primary('id');
-            $table->string('old_id');
+            $table->string('old_id')->nullable();
             $table->string('serial_number')->nullable();
             $table->enum('status', ['Good', 'Fine', 'Bad', 'Damaged']);
             $table->string('description')->nullable();
             $table->boolean('in_service')->default(1);
             $table->integer('real_price')->nullable();
             $table->integer('expected_price')->nullable();
-            $table->date('acquisition_date');
+            $table->date('acquisition_date')->nullable();
             $table->enum('acquisition_type', ['Directed', 'Founded', 'Transferred']);
             $table->string('funded_by')->nullable();
             $table->string('note')->nullable();
