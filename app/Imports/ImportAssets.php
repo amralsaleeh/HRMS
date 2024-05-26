c9944afe60e808f2058fd1365dfdcdfb72f11412
Amr Alsaleh <amralsaleeh@outlook.com>
2024-05-26 13:12:07 +0300

    Edit assets import method

--- app/Imports/ImportAssets.php
+++ app/Imports/ImportAssets.php
@@ -8,59 +8,71 @@ use App\Models\SubCategory;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ImportAssets implements ToModel, WithStartRow
+class ImportAssets implements ToModel, WithHeadingRow
 {
-    public function startRow(): int
-    {
-        return 2;
-    }
-
     public function model(array $row)
     {
+        // dd($row);
         // 👉 Assets
-        $assetsText = $row[3];
-        $parts = explode('_-_', $assetsText);
+        Asset::firstOrCreate([
+            'id' => $row['id'],
+            'old_id' => null,
+            'serial_number' => $row['serial_number'],
+            'status' => $row['status'],
+            'description' => null,
+            'in_service' => $row['in_service'],
+            'is_gpr' => $row['is_gpr'],
+            'real_price' => $row['real_price'],
+            'expected_price' => null,
+            'acquisition_date' => null,
+            'acquisition_type' => $row['acquisition_type'],
+            'funded_by' => $row['funded_by'],
+            'note' => $row['note'],
+        ]);
 
-        $categoryText = $parts[0];
-        $subCategoryText = $parts[1];
+        // $assetsText = $row[3];
+        // $parts = explode('_-_', $assetsText);
 
-        $old_code = $row[5];
-        $quantity = $row[4];
+        // $categoryText = $parts[0];
+        // $subCategoryText = $parts[1];
 
-        $category = Category::where('name', $categoryText)->first();
-        if (! $category) {
-            info($categoryText.' Category - NOT FOUND!!');
-        }
+        // $old_code = $row[5];
+        // $quantity = $row[4];
 
-        $subCategory = SubCategory::where('name', $subCategoryText)->first();
-        if (! $subCategory) {
-            info($subCategory.$row.' SubCategory - NOT FOUND!!');
-        }
+        // $category = Category::where('name', $categoryText)->first();
+        // if (! $category) {
+        //     info($categoryText.' Category - NOT FOUND!!');
+        // }
 
-        $category_id = str_pad($category->id, 4, '0', STR_PAD_LEFT);
-        $subCategory_id = str_pad($subCategory->id, 4, '0', STR_PAD_LEFT);
+        // $subCategory = SubCategory::where('name', $subCategoryText)->first();
+        // if (! $subCategory) {
+        //     info($subCategory.$row.' SubCategory - NOT FOUND!!');
+        // }
 
-        for ($i = 1; $i <= $quantity; $i++) {
-            $loop = str_pad($i, 4, '0', STR_PAD_LEFT);
-            $new_code = "$category_id$subCategory_id$loop";
+        // $category_id = str_pad($category->id, 4, '0', STR_PAD_LEFT);
+        // $subCategory_id = str_pad($subCategory->id, 4, '0', STR_PAD_LEFT);
 
-            Asset::firstOrCreate([
-                'id' => $new_code,
-                'old_id' => $old_code,
-                'serial_number' => null,
-                'status' => 'Good',
-                'description' => null,
-                'in_service' => 1,
-                'real_price' => null,
-                'expected_price' => null,
-                'acquisition_date' => Carbon::now(),
-                'acquisition_type' => 'Directed',
-                'funded_by' => null,
-                'note' => null,
-            ]);
-        }
+        // for ($i = 1; $i <= $quantity; $i++) {
+        //     $loop = str_pad($i, 4, '0', STR_PAD_LEFT);
+        //     $new_code = "$category_id$subCategory_id$loop";
+
+        //     Asset::firstOrCreate([
+        //         'id' => $new_code,
+        //         'old_id' => $old_code,
+        //         'serial_number' => null,
+        //         'status' => 'Good',
+        //         'description' => null,
+        //         'in_service' => 1,
+        //         'real_price' => null,
+        //         'expected_price' => null,
+        //         'acquisition_date' => Carbon::now(),
+        //         'acquisition_type' => 'Directed',
+        //         'funded_by' => null,
+        //         'note' => null,
+        //     ]);
+        // }
 
         // 👉 Category
         // Category::firstOrCreate([
