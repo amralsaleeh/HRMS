98c3bd859966465da90287993bdc9c7a0d01e498
Amr Alsaleh <amralsaleeh@outlook.com>
2024-05-27 11:52:57 +0300

    Fix config issue

--- config/webhook-client.php
+++ config/webhook-client.php
@@ -18,7 +18,7 @@ return [
             /*
              * The name of the header containing the signature.
              */
-            'signature_header_name' => 'Signature',
+            'signature_header_name' => 'X-Hub-Signature',
 
             /*
              *  This class will verify that the content of the signature header is valid.
