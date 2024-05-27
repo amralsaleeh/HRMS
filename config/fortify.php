09c1a7ef419aaeb2dd1f1ecb2dc81f1936a96fb0
Amr Alsaleh <amralsaleeh@outlook.com>
2024-05-27 13:26:32 +0300

    Turn off account features

--- config/fortify.php
+++ config/fortify.php
@@ -147,12 +147,12 @@ return [
         Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
+        // Features::updateProfileInformation(),
+        // Features::updatePasswords(),
+        // Features::twoFactorAuthentication([
+        //     'confirm' => true,
+        //     'confirmPassword' => true,
+        //     // 'window' => 0,
+        // ]),
     ],
 ];
