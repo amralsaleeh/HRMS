0ff61fa1a8a3258bb87fcfb76bcbac2a5d21eb52
Amr Alsaleh <amralsaleeh@outlook.com>
2024-06-04 13:45:22 +0300

    Enable update password features

--- config/fortify.php
+++ config/fortify.php
@@ -148,7 +148,7 @@ return [
         Features::resetPasswords(),
         // Features::emailVerification(),
         // Features::updateProfileInformation(),
-        // Features::updatePasswords(),
+        Features::updatePasswords(),
         // Features::twoFactorAuthentication([
         //     'confirm' => true,
         //     'confirmPassword' => true,
