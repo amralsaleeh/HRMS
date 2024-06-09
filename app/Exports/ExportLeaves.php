166730e804171251aa94ffa4efd6fceb1654962e
Amr Alsaleh <amralsaleeh@outlook.com>
2024-06-09 13:33:11 +0300

    Enable export functionality

---
+++ app/Exports/ExportLeaves.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class ExportLeaves implements FromCollection, ShouldAutoSize, WithHeadings, WithStyles
+{
+    private $headings;
+
+    private $data;
+
+    public function __construct($leaves)
+    {
+        $this->data = $leaves;
+        $this->headings = $this->generateHeadings();
+    }
+
+    public function headings(): array
+    {
+        return $this->headings;
+    }
+
+    protected function generateHeadings()
+    {
+        if (empty($this->data)) {
+            return [];
+        }
+
+        return array_keys((array) $this->data[0]);
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1 => [
+                'font' => [
+                    'bold' => true,
+                    'size' => 16],
+                'color' => ['hex' => '808080'],
+                'alignment' => [
+                    'horizontal' => Alignment::HORIZONTAL_CENTER,
+                    'vertical' => Alignment::VERTICAL_CENTER,
+                    'wrapText' => true,
+                ],
+            ],
+        ];
+    }
+
+    public function collection()
+    {
+        return collect($this->data);
+    }
+}
