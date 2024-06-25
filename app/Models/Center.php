06482b3c24a1daca0bd9d3742094fbec02a3d0b2
Amr Alsaleh <amralsaleeh@outlook.com>
2024-06-25 15:30:22 +0300

    Show all employees to HR role

--- app/Models/Center.php
+++ app/Models/Center.php
@@ -10,17 +10,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class Center extends Model
 {
     use CreatedUpdatedDeletedBy, HasFactory, SoftDeletes;
 
-    protected $fillable = [
-        'name',
-        'start_work_hour',
-        'end_work_hour',
-        'weekends',
-    ];
+    protected $fillable = ['name', 'start_work_hour', 'end_work_hour', 'weekends'];
 
     public function timelines(): HasMany
     {
@@ -34,50 +30,64 @@ class Center extends Model
 
     public function activeEmployees()
     {
-        $centerEmployees = $this->timelines()
-            ->whereNull('end_date')
-            ->join('employees', 'timelines.employee_id', '=', 'employees.id')
-            ->orderBy('employees.first_name', 'asc')
-            ->with('employee')->get();
+        if (
+            in_array(
+                User::find(Auth::id())
+                    ?->getRoleNames()
+                    ->first(),
+                ['Admin', 'HR']
+            )
+        ) {
+            $activeEmployees = Timeline::whereNull('end_date')
+                ->join('employees', 'timelines.employee_id', '=', 'employees.id')
+                ->orderBy('employees.first_name', 'asc')
+                ->with('employee')
+                ->get();
+        } else {
+            $centerEmployees = $this->timelines()
+                ->whereNull('end_date')
+                ->join('employees', 'timelines.employee_id', '=', 'employees.id')
+                ->orderBy('employees.first_name', 'asc')
+                ->with('employee')
+                ->get();
 
-        $notAffiliatedEmployees = Center::find(100)->timelines()
-            ->whereNull('end_date')
-            ->join('employees', 'timelines.employee_id', '=', 'employees.id')
-            ->orderBy('employees.first_name', 'asc')
-            ->with('employee')->get();
+            $notAffiliatedEmployees = Center::find(100)
+                ->timelines()
+                ->whereNull('end_date')
+                ->join('employees', 'timelines.employee_id', '=', 'employees.id')
+                ->orderBy('employees.first_name', 'asc')
+                ->with('employee')
+                ->get();
 
-        $mergedEmployees = $centerEmployees->merge($notAffiliatedEmployees);
-        $mergedEmployees = $mergedEmployees->sortBy('employee.first_name');
+            $mergedEmployees = $centerEmployees->merge($notAffiliatedEmployees);
+            $mergedEmployees = $mergedEmployees->sortBy('employee.first_name');
 
-        return $mergedEmployees;
+            $activeEmployees = $mergedEmployees;
+        }
+
+        return $activeEmployees;
     }
 
     protected function name(): Attribute
     {
-        return Attribute::make(
-            set: fn (string $value) => ucfirst($value),
-        );
+        return Attribute::make(set: fn (string $value) => ucfirst($value));
     }
 
     protected function startWorkHour(): Attribute
     {
-        return Attribute::make(
-            get: fn (string $value) => Carbon::parse($value)->format('H:i'),
-        );
+        return Attribute::make(get: fn (string $value) => Carbon::parse($value)->format('H:i'));
     }
 
     protected function endWorkHour(): Attribute
     {
-        return Attribute::make(
-            get: fn (string $value) => Carbon::parse($value)->format('H:i'),
-        );
+        return Attribute::make(get: fn (string $value) => Carbon::parse($value)->format('H:i'));
     }
 
     protected function weekends(): Attribute
     {
         return Attribute::make(
             get: fn (string $value) => explode(',', $value),
-            set: fn (array $value) => implode(',', $value),
+            set: fn (array $value) => implode(',', $value)
         );
     }
 }
