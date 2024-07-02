93653c5ebfa2fd864323c496a003dc262a6af851
Amr Alsaleh <amralsaleeh@outlook.com>
2024-07-02 15:13:34 +0300

    Reorder timelines

--- app/Livewire/HumanResource/Structure/EmployeeInfo.php
+++ app/Livewire/HumanResource/Structure/EmployeeInfo.php
@@ -41,16 +41,16 @@ class EmployeeInfo extends Component
     public function mount($id)
     {
         $this->employee = Employee::find($id);
-        $this->timelines = Timeline::with(['center', 'department', 'position'])->where('employee_id', $id)->get();
-        // dd($this->timeline);
-        $this->position = Timeline::with('position')->where('employee_id', $id)->first();
+        $this->timelines = Timeline::with(['center', 'department', 'position'])
+            ->where('employee_id', $id)
+            ->orderBy('id', 'desc')
+            ->get();
+        $this->position = Timeline::with('position')
+            ->where('employee_id', $id)
+            ->first();
         $this->positions = Position::all();
         $this->Centers = Center::all();
         $this->departments = department::all();
-        $this->dispatch('closeModal', elementId: '#employeeinfoModal');
-        $this->dispatch('toastr', type: 'success'/* , title: 'Done!' */ , message: 'Going Well!');
-
-        // dd($this->posation->start_date);
     }
 
     public function render()
@@ -71,8 +71,8 @@ class EmployeeInfo extends Component
 
         Timeline::create([
             'employee_id' => $this->employee->id,
-            'position_id' =>$this->employeeInfo['position'],
-            'department_id'=>$this->employeeInfo['department'],
+            'position_id' => $this->employeeInfo['position'],
+            'department_id' => $this->employeeInfo['department'],
             'center_id' => $this->employeeInfo['center'],
             'start_date' => $this->employeeInfo['start_date'],
             'end_date' => $this->employeeInfo['end_date'],
@@ -81,6 +81,6 @@ class EmployeeInfo extends Component
         ]);
 
         $this->dispatch('closeModal', elementId: '#add-timeline');
-        $this->dispatch('toastr', type: 'success'/* , title: 'Done!' */ , message: 'Going Well!');
+        $this->dispatch('toastr', type: 'success' /* , title: 'Done!' */, message: 'Going Well!');
     }
 }
