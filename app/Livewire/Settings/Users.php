3ce85131e58667e714fa331fa8d28fe794781f2e
Amr Alsaleh <amralsaleeh@outlook.com>
2024-07-08 11:32:08 +0300

    Add users page

---
+++ app/Livewire/Settings/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Settings;
+
+use Livewire\Component;
+
+class Users extends Component
+{
+    public function render()
+    {
+        return view('livewire.settings.users');
+    }
+}
