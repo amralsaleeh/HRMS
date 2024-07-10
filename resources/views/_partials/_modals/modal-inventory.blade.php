8e11127cbbea620b828bf2f256160cd91a7d40d8
Amr Alsaleh <amralsaleeh@outlook.com>
2024-07-10 11:19:23 +0300

    Improve UI

--- resources/views/_partials/_modals/modal-inventory.blade.php
+++ resources/views/_partials/_modals/modal-inventory.blade.php
@@ -26,23 +26,19 @@
           <p class="text-muted">{{ __('Please fill out the following information') }}</p>
         </div>
         <form wire:submit="submitAsset" class="row g-3">
-          <div class="col-md-6 col-12 mb-4">
+          <div class="col-md-2 col-12">
             <label class="form-label w-100">{{ __('ID') }}</label>
             <input wire:model='assetId' class="form-control @error('assetId') is-invalid @enderror" type="number" />
           </div>
-          <div class="col-md-6 col-12 mb-4">
+          <div class="col-md-2 col-12">
             <label class="form-label w-100">{{ __('Old ID') }}</label>
             <input wire:model='oldId' class="form-control @error('oldId') is-invalid @enderror" type="number" />
           </div>
-          <div class="col-md-6 col-12 mb-4">
+          <div class="col-md-2 col-12">
             <label class="form-label w-100">{{ __('Serial Number') }}</label>
             <input wire:model='serialNumber' class="form-control @error('serialNumber') is-invalid @enderror" type="text" />
           </div>
-          <div class="col-md-6 col-12 mb-4">
-            <label class="form-label w-100">{{ __('Description') }}</label>
-            <input wire:model='description' class="form-control @error('description') is-invalid @enderror" type="text" />
-          </div>
-          <div class="col-md-6 col-12 mb-4">
+          <div class="col-md-2 col-12">
             <label class="form-label w-100">{{ __('Status') }}</label>
             <select wire:model='status' class="form-control @error('status') is-invalid @enderror" data-allow-clear="true">
               <option value=""></option>
@@ -52,7 +48,7 @@
               <option value="Damaged">{{ __('Damaged') }}</option>
             </select>
           </div>
-          <div class="col-md-6 col-12 mb-4">
+          <div class="col-md-2 col-12">
             <label class="form-label w-100">{{ __('In Service') }}</label>
             <select wire:model='inService'  class="form-control @error('inService') is-invalid @enderror" data-allow-clear="true">
               <option value=""></option>
@@ -60,28 +56,7 @@
               <option value="0">{{ __('Inactive') }}</option>
             </select>
           </div>
-          <div class="col-md-6 col-12 mb-4">
-            <label class="form-label w-100">{{ __('Real Price') }}</label>
-            <input wire:model='realPrice' class="form-control @error('realPrice') is-invalid @enderror" type="number" />
-          </div>
-          <div class="col-md-6 col-12 mb-4">
-            <label class="form-label w-100">{{ __('Expected Price') }}</label>
-            <input wire:model='expectedPrice' class="form-control @error('expectedPrice') is-invalid @enderror" type="number" />
-          </div>
-          <div class="col-md-6 col-12 mb-4">
-            <label class="form-label w-100">{{ __('Acquisition Date') }}</label>
-            <input wire:model='acquisitionDate' class="form-control flatpickr-input active @error('acquisitionDate') is-invalid @enderror" type="text" id="flatpickr-Acquisition-Date" readonly="readonly"/>
-          </div>
-          <div class="col-md-6 col-12 mb-4">
-            <label class="form-label w-100">{{ __('Acquisition Type') }}</label>
-            <select wire:model='acquisitionType' class="form-control @error('acquisitionType') is-invalid @enderror" data-allow-clear="true">
-              <option value=""></option>
-              <option value="Transferred">{{ __('Transferred') }}</option>
-              <option value="Directed">{{ __('Directed') }}</option>
-              <option value="Founded">{{ __('Founded') }}</option>
-            </select>
-          </div>
-          <div class="col-md-6 col-12 mb-4">
+          <div class="col-md-2 col-12">
             <label class="form-label w-100">{{ __('Founded By') }}</label>
             <select wire:model='fundedBy' class="form-control @error('fundedBy') is-invalid @enderror" data-allow-clear="true">
               <option value=""></option>
@@ -90,7 +65,32 @@
               <option value="By Taalouf">{{ __('By Taalouf') }}</option>
             </select>
           </div>
-          <div class="col-md-6 col-12 mb-4">
+          <div class="col-md-2 col-12">
+            <label class="form-label w-100">{{ __('Real Price') }}</label>
+            <input wire:model='realPrice' class="form-control @error('realPrice') is-invalid @enderror" type="number" />
+          </div>
+          <div class="col-md-2 col-12">
+            <label class="form-label w-100">{{ __('Expected Price') }}</label>
+            <input wire:model='expectedPrice' class="form-control @error('expectedPrice') is-invalid @enderror" type="number" />
+          </div>
+          <div class="col-md-2 col-12">
+            <label class="form-label w-100">{{ __('Acquisition Type') }}</label>
+            <select wire:model='acquisitionType' class="form-control @error('acquisitionType') is-invalid @enderror" data-allow-clear="true">
+              <option value=""></option>
+              <option value="Transferred">{{ __('Transferred') }}</option>
+              <option value="Directed">{{ __('Directed') }}</option>
+              <option value="Founded">{{ __('Founded') }}</option>
+            </select>
+          </div>
+          <div class="col-md-2 col-12">
+            <label class="form-label w-100">{{ __('Acquisition Date') }}</label>
+            <input wire:model='acquisitionDate' class="form-control flatpickr-input active @error('acquisitionDate') is-invalid @enderror" type="text" id="flatpickr-Acquisition-Date" readonly="readonly"/>
+          </div>
+          <div class="col-md-4 col-12">
+            <label class="form-label w-100">{{ __('Description') }}</label>
+            <input wire:model='description' class="form-control @error('description') is-invalid @enderror" type="text" />
+          </div>
+          <div class="col-md-12 col-12 mb-4">
             <label class="form-label w-100">{{ __('Note') }}</label>
             <input wire:model='note' class="form-control @error('note') is-invalid @enderror" type="text" />
           </div>
