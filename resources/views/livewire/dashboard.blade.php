9bd58aecac991436c02edbaf5af95f9189099750
Amr Alsaleh <amralsaleeh@outlook.com>
2024-07-17 13:35:28 +0300

    Improve localization

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -83,7 +83,7 @@
           <div class="d-flex justify-content-between mb-3">
             <h5 class="card-title mb-0">{{ __('Statistics') }}</h5>
             @can('read sms')
-              <small class="text-muted">{{ $accountBalance['status'] == 200 ? 'Updated recently' : 'Error, Update unavailable' }}</small>
+              <small class="text-muted">{{ $accountBalance['status'] == 200 ? __('Updated recently') : __('Error, Update unavailable') }}</small>
             @endcan
           </div>
         </div>
