4bf7298bca5b2200f3a6b157190fdfe36e2833a8
Amr Alsaleh <amralsaleeh@outlook.com>
2024-07-25 13:09:19 +0300

    Initialization

--- app/Imports/ImportAssets.php
+++ app/Imports/ImportAssets.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Asset;
 use App\Models\Category;
 use App\Models\SubCategory;
+use App\Models\Transition;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\ToModel;
@@ -42,42 +43,80 @@ class ImportAssets implements ToModel, WithHeadingRow
         // }
 
         // 👉 Assets
-        $quantity = 0;
+        // $quantity = 0;
 
+        // $init = 1;
+        // $category_id = str_pad($row['category_id'], 4, '0', STR_PAD_LEFT);
+        // $subCategory_id = str_pad($row['sub_category_id'], 4, '0', STR_PAD_LEFT);
+        // $quantity = str_pad(1, 4, '0', STR_PAD_LEFT);
+
+        // $lastRecord = Asset::where('id', $init.$category_id.$subCategory_id.$quantity)->first();
+
+        // if (! $lastRecord) {
+        //     Asset::firstOrCreate([
+        //         'id' => $init.$category_id.$subCategory_id.$quantity,
+        //         'old_id' => $row['old_id'],
+        //         'serial_number' => $row['serial_number'],
+        //         'status' => $row['status'],
+        //         'description' => $row['description'],
+        //         'in_service' => $row['in_service'],
+        //         'is_gpr' => 0,
+        //         'real_price' => $row['real_price'],
+        //         'expected_price' => $row['expected_price'],
+        //         'acquisition_date' => $row['acquisition_date'],
+        //         'acquisition_type' => $row['acquisition_type'],
+        //         'funded_by' => $row['funded_by'],
+        //         'note' => $row['note'],
+        //     ]);
+        // } else {
+        //     $quantity = Asset::where('id', 'like', $init.$category_id.$subCategory_id.'%')->count();
+        //     $quantity++;
+        //     $quantity = str_pad($quantity, 4, '0', STR_PAD_LEFT);
+
+        //     Asset::firstOrCreate([
+        //         'id' => $init.$category_id.$subCategory_id.$quantity,
+        //         'old_id' => $row['old_id'],
+        //         'serial_number' => $row['serial_number'],
+        //         'status' => $row['status'],
+        //         'description' => $row['description'],
+        //         'in_service' => $row['in_service'],
+        //         'is_gpr' => 0,
+        //         'real_price' => $row['real_price'],
+        //         'expected_price' => $row['expected_price'],
+        //         'acquisition_date' => $row['acquisition_date'],
+        //         'acquisition_type' => $row['acquisition_type'],
+        //         'funded_by' => $row['funded_by'],
+        //         'note' => $row['note'],
+        //     ]);
+        // }
+
+        // 👉 Inventory
         $init = 1;
         $category_id = str_pad($row['category_id'], 4, '0', STR_PAD_LEFT);
         $subCategory_id = str_pad($row['sub_category_id'], 4, '0', STR_PAD_LEFT);
         $quantity = str_pad(1, 4, '0', STR_PAD_LEFT);
 
-        $lastRecord = Asset::where('id', $init.$category_id.$subCategory_id.$quantity)->first();
+        $generatedId = $init.$category_id.$subCategory_id.$quantity;
 
-        if (! $lastRecord) {
-            Asset::firstOrCreate([
-                'id' => $init.$category_id.$subCategory_id.$quantity,
-                'old_id' => $row['old_id'],
-                'serial_number' => $row['serial_number'],
-                'status' => $row['status'],
-                'description' => $row['description'],
-                'in_service' => $row['in_service'],
-                'is_gpr' => 0,
-                'real_price' => $row['real_price'],
-                'expected_price' => $row['expected_price'],
-                'acquisition_date' => $row['acquisition_date'],
-                'acquisition_type' => $row['acquisition_type'],
-                'funded_by' => $row['funded_by'],
-                'note' => $row['note'],
-            ]);
+        $asset = Asset::find($generatedId);
+        if ($asset) {
+            $transition = Transition::where('asset_id', $asset->id)
+                ->latest()
+                ->first();
+
+            dd($transition);
         } else {
-            $quantity = Asset::where('id', 'like', $init.$category_id.$subCategory_id.'%')->count();
-            $quantity++;
-            $quantity = str_pad($quantity, 4, '0', STR_PAD_LEFT);
+            $init = 1;
+            $category_id = str_pad($row['category_id'], 4, '0', STR_PAD_LEFT);
+            $subCategory_id = str_pad($row['sub_category_id'], 4, '0', STR_PAD_LEFT);
+            $quantity = str_pad(1, 4, '0', STR_PAD_LEFT);
 
-            Asset::firstOrCreate([
+            Asset::create([
                 'id' => $init.$category_id.$subCategory_id.$quantity,
                 'old_id' => $row['old_id'],
-                'serial_number' => $row['serial_number'],
-                'status' => $row['status'],
-                'description' => $row['description'],
+                'serial_number' => null,
+                'status' => 'Fine',
+                'description' => null,
                 'in_service' => $row['in_service'],
                 'is_gpr' => 0,
                 'real_price' => $row['real_price'],
