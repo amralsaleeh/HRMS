225280ffef99dd6b06699e3877302df8c63c0538
Amr Alsaleh <amralsaleeh@outlook.com>
2025-03-10 11:09:51 +0300

    Extend FromDateLimit

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -78,7 +78,7 @@ class Dashboard extends Component
         }
 
         $this->fromDateLimit = Carbon::now()
-            ->subDays(30)
+            ->subDays(90) //TODO: Change it back to 30 day only
             ->format('Y-m-d');
         $this->changelogs = Changelog::latest()->get();
     }
