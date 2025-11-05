869abbbcf3660f523410a1b85d03cddfb0d4e2ec
Amr Alsaleh <amralsaleeh@outlook.com>
2025-11-05 14:49:15 +0300

    Change leave balance badge color from success to warning

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -186,7 +186,7 @@
               </div> --}}
               <div class="col-md-3 col-6">
                 <div class="d-flex align-items-center">
-                  <div class="badge rounded-pill bg-label-success me-3 p-2"><i class="ti ti-zzz ti-sm"></i></div>
+                  <div class="badge rounded-pill bg-label-warning me-3 p-2"><i class="ti ti-zzz ti-sm"></i></div>
                   <div class="card-info">
                     <h5 class="mb-0">{{ $employee->max_leave_allowed }}</h5>
                     <small>{{ __('Leaves Balance') }}</small>
