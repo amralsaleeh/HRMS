a605f15a49b6e4c2b1aef3f1338b6457f5256cd4
Amr Alsaleh <amralsaleeh@outlook.com>
2025-11-12 11:01:01 +0300

    Update contact-us page

--- resources/views/livewire/contact-us.blade.php
+++ resources/views/livewire/contact-us.blade.php
@@ -28,29 +28,55 @@
     <div class="col-12 col-md-4 mb-4">
       <div style="text-align: center">
         <h5 class="card-title">Amr Alsaleh</h5>
-        <h6 class="card-subtitle text-muted">Software Engineer</h6>
+        <h6 class="card-subtitle text-muted mb-4">Software Engineer</h6>
+        <div style="display: flex; justify-content: center;">
+          <div class="badge-base LI-profile-badge"
+              data-locale="en_US"
+              data-size="large"
+              data-theme="light"
+              data-type="HORIZONTAL"
+              data-vanity="amralsaleeh"
+              data-version="v1">
+          </div>
+        </div>
       </div>
       <br>
-      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="amralsaleeh" data-version="v1">
-      </div>
     </div>
+
     <div class="col-12 col-md-4 mb-4">
       <div style="text-align: center">
         <h5 class="card-title">Sewar Khalil</h5>
-        <h6 class="card-subtitle text-muted">Data Analyst</h6>
-        <br>
+        <h6 class="card-subtitle text-muted mb-4">Data Analyst</h6>
+        <div style="display: flex; justify-content: center;">
+          <div class="badge-base LI-profile-badge"
+              data-locale="en_US"
+              data-size="large"
+              data-theme="light"
+              data-type="HORIZONTAL"
+              data-vanity="sewar-khalil"
+              data-version="v1">
+          </div>
+        </div>
       </div>
-      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="sewar-khalil" data-version="v1">
-      </div>
+      <br>
     </div>
+
     <div class="col-12 col-md-4 mb-4">
       <div style="text-align: center">
         <h5 class="card-title">Mohammad Habib</h5>
-        <h6 class="card-subtitle text-muted">Data Analyst</h6>
-        <br>
+        <h6 class="card-subtitle text-muted mb-4">Data Analyst</h6>
+        <div style="display: flex; justify-content: center;">
+          <div class="badge-base LI-profile-badge"
+              data-locale="en_US"
+              data-size="large"
+              data-theme="light"
+              data-type="HORIZONTAL"
+              data-vanity="mohammad-habib-21337a171"
+              data-version="v1">
+          </div>
+        </div>
       </div>
-      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="mohammad-habib-21337a171" data-version="v1">
-      </div>
+      <br>
     </div>
   </div>
 
