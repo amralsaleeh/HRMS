8da5fcab1098995c23f7631d39ddd89aad07f5f7
Amr Alsaleh <amralsaleeh@outlook.com>
2025-11-12 10:53:47 +0300

    Update contact-us page

--- resources/views/livewire/contact-us.blade.php
+++ resources/views/livewire/contact-us.blade.php
@@ -25,7 +25,7 @@
   </nav> --}}
 
   <div class="row">
-    <div class="col-4">
+    <div class="col-12 col-md-4 mb-4">
       <div style="text-align: center">
         <h5 class="card-title">Amr Alsaleh</h5>
         <h6 class="card-subtitle text-muted">Software Engineer</h6>
@@ -34,22 +34,22 @@
       <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="amralsaleeh" data-version="v1">
       </div>
     </div>
-    {{-- <div class="col-4">
-      <div style="text-align: center">
-        <h5 class="card-title">Aref Naser Agha</h5>
-        <h6 class="card-subtitle text-muted">Network Administration</h6>
-        <br>
-      </div>
-      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="aref-naser-agha" data-version="v1">
-      </div>
-    </div> --}}
-    <div class="col-4">
+    <div class="col-12 col-md-4 mb-4">
       <div style="text-align: center">
         <h5 class="card-title">Sewar Khalil</h5>
         <h6 class="card-subtitle text-muted">Data Analyst</h6>
         <br>
       </div>
       <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="sewar-khalil" data-version="v1">
+      </div>
+    </div>
+    <div class="col-12 col-md-4 mb-4">
+      <div style="text-align: center">
+        <h5 class="card-title">Mohammad Habib</h5>
+        <h6 class="card-subtitle text-muted">Data Analyst</h6>
+        <br>
+      </div>
+      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="mohammad-habib-21337a171" data-version="v1">
       </div>
     </div>
   </div>
