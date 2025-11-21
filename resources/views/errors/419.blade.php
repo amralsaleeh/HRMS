0b5140ced75b7adf6b391ef3191a1c49a118de71
Amr Alsaleh <amralsaleeh@outlook.com>
2025-11-22 00:20:31 +0300

    Improve localization

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -16,7 +16,7 @@
   <div class="misc-wrapper">
     <h2 class="mb-1 mt-4">{{ __('Page Expired ⏳') }}</h2>
     <p class="mb-4 mx-2">{{ __('It seems the page has expired or the request was submitted incorrectly. Please try reloading the page.') }}</p>
-    <a href="{{ url()->previous() }}" class="btn btn-primary mb-4">{{ __('Go Back') }}</a>
+    <a href="{{ url()->previous() }}" class="btn btn-primary mb-4">{{ __('Go back') }}</a>
     <div class="mt-4">
       <img src="{{ asset('assets/img/illustrations/page-misc-error.png') }}" alt="page-misc-error" width="150" class="img-fluid">
     </div>
